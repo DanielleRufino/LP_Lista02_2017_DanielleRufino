ec30151a79c61e1e28841b9dc396285113714b9f
DanielleRufino <danielleassisr@gmail.com>
2017-07-18 21:47:34 -0300

    Create Questao6letrab.java

---
+++ Questao6letrab.java
@@ -0,0 +1,19 @@
+package parImpar;
+
+import java.util.Scanner;
+
+public class ParImpar {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner numero = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        n = numero.nextInt();
+        if ((n%2) == 0){
+            System.out.println("O valor " + n + " é Impar.");
+        } else {
+            System.out.println("O valor " + n + " é Par.");
+        }
+    }
+
+}
